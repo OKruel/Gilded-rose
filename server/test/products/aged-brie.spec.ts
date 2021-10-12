5fb568a0e83d6e47902c32a833c742c5c6a72baa
Olavo <olavokruel@gmail.com>
2021-10-13 00:33:43 +0200

    WIP: First refactor of products sulfuras and unregistered

--- server/test/products/aged-brie.spec.ts
+++ server/test/products/aged-brie.spec.ts
@@ -34,8 +34,8 @@ describe('PRODUCT -> AGED BRIE', function () {
 	});
 
 	it('If selling day has NOT passed (SellIn > 0), and Quality > 49 => Quality continue the same and SellIn - 1.', function () {
-		const testStoredProduct = [new Item(productNames.brie, -1, 50)];
-		const testExpectedOutcome = [new Item(productNames.brie, -2, 50)];
+		const testStoredProduct = [new Item(productNames.brie, 2, 50)];
+		const testExpectedOutcome = [new Item(productNames.brie, 1, 50)];
 
 		const gildedRoseShop = new GildedRose(testStoredProduct);
 		const items = gildedRoseShop.updateQuality();
@@ -44,8 +44,14 @@ describe('PRODUCT -> AGED BRIE', function () {
 	});
 
 	it('If selling day has NOT passed (SellIn > 0), and Quality < 50 => Quality + 1 and SellIn - 1.', function () {
-		const testStoredProduct = [new Item(productNames.brie, -1, 49)];
-		const testExpectedOutcome = [new Item(productNames.brie, -2, 50)];
+		const testStoredProduct = [
+			new Item(productNames.brie, 2, 49),
+			new Item(productNames.brie, 2, -49),
+		];
+		const testExpectedOutcome = [
+			new Item(productNames.brie, 1, 50),
+			new Item(productNames.brie, 1, -48),
+		];
 
 		const gildedRoseShop = new GildedRose(testStoredProduct);
 		const items = gildedRoseShop.updateQuality();
--- server/test/products/unregistered.spec.ts
+++ server/test/products/unregistered.spec.ts
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
-import { Item, GildedRose } from '../../app/gilded-rose';
+import { Item, GildedRose } from '../../app/refactor';
 import { productNames } from '../../app/constants';
 
 describe('PRODUCT -> UNREGISTERED', function () {
-	it('Quality > 0 && SellIn > 0 ===> Quality subtracts -1 -- SellIn subtracts -1', function () {
+	it('If selling day has NOT passed (SellIn > 0) and Quality > 0 ==> Quality - 1 and SellIn - 1', function () {
 		const testStoredProduct = [new Item(productNames.unregistered, 1, 51)];
 		const testExpectedOutcome = [new Item(productNames.unregistered, 0, 50)];
 
@@ -13,7 +13,7 @@ describe('PRODUCT -> UNREGISTERED', function () {
 		expect(items).deep.equal(testExpectedOutcome);
 	});
 
-	it('Quality > 0 && SellIn < 0 ===> Quality subtracts -2 -- SellIn subtracts -1', function () {
+	it('If selling day has passed (SellIn < 0) and Quality > 0 ==> Quality - 2 and SellIn - 1', function () {
 		const testStoredProduct = [new Item(productNames.unregistered, -1, 49)];
 		const testExpectedOutcome = [new Item(productNames.unregistered, -2, 47)];
 
